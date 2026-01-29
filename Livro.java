455b9a0ea7a80cbeb3f397077e09aaf8bbd6cb6f
Bernardo Gimbi <bergimbirgmail.com>
2026-01-29 22:43:16 +0000

    Adiciona Metodo "mostrarDetalhes"

--- Livro.java
+++ Livro.java
@@ -3,7 +3,13 @@ package Livro;
 
 public class Livro {
     String nome;
-    String derscricao;
+    String descricao;
     String isbn;
     double valor;
+    void mostrarDetalhes() {
+        System.out.println (nome);
+        System.out.println(descricao);
+        System.out.println(valor);
+        System.out.println(isbn);
+    }
 }
